6d284df3db534332bb73b9247571fabf138bc13e
MaDRaGe <locodi111@mail.ru>
2020-10-14 20:11:55 +0300

    Updates

---
+++ precache-manifest.1e5bb2a75f6acb3376a24e4e232722c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89453122ddc64b2044f206235e4e0ec",
+    "url": "/ivi-react-ts/index.html"
+  },
+  {
+    "revision": "e25f741dd42fa846b518",
+    "url": "/ivi-react-ts/static/css/main.f28f46a7.chunk.css"
+  },
+  {
+    "revision": "92fa1e591d0af467f3fa",
+    "url": "/ivi-react-ts/static/js/2.414adff3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ivi-react-ts/static/js/2.414adff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25f741dd42fa846b518",
+    "url": "/ivi-react-ts/static/js/main.9426ad16.chunk.js"
+  },
+  {
+    "revision": "f2570d48a5615add2a08",
+    "url": "/ivi-react-ts/static/js/runtime-main.2c5ab427.js"
+  },
+  {
+    "revision": "dbcd1ecf1a4b0d74289a54ed72edc1c3",
+    "url": "/ivi-react-ts/static/media/ArchivoNarrow-Regular.dbcd1ecf.ttf"
+  }
+]);
